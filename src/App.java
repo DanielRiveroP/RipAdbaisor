7ed638f5f023f667fddef02d95e80731f18efa3e
DanielRiveroP <riveroperezmesa@gmail.com>
2026-05-14 18:10:07 +0100

    Añadida la opcion de añadir restaurante

--- src/App.java
+++ src/App.java
@@ -10,17 +10,25 @@ public class App {
 
         do {
             System.out.println("\n--- BIENVENIDO A RIPADBAISOR ---");
-            System.out.println("a. Añadir restaurante");
-            System.out.println("b. Editar restaurante");
-            System.out.println("c. Mostrar ranking (descendente)");
-            System.out.println("d. Eliminar restaurante");
-            System.out.println("e. Salir");
+            System.out.println("1. Añadir restaurante");
+            System.out.println("2. Editar restaurante");
+            System.out.println("3. Mostrar ranking (descendente)");
+            System.out.println("4. Eliminar restaurante");
+            System.out.println("5. Salir");
 
             opcion = Validaciones.leerEntero("Seleccione una opción entre 1 y 5");
             switch (opcion) {
                 case 1:
-                    System.out.println("Has elegido: Añadir");
-                    // Aquí llamaremos a la lógica de añadir en la siguiente rama
+                    System.out.println("\n Nuevo Registro");
+                    
+                    String nombre = Validaciones.leerTexto("Ingrese el nombre del restaurante");
+                    String direccion = Validaciones.leerTexto("Ingrese la dirección del restaurante");
+                    String tipoCocina = Validaciones.leerTexto("Ingrese el tipo de cocina del restaurante");
+                    double puntuacion = Validaciones.leerDouble("Ingrese la puntuación del restaurante (0.0 - 5.0)");
+
+                    Restaurante nuevoRestaurante = new Restaurante(nombre, direccion, tipoCocina, puntuacion);
+                    listaRestaurantes.add(nuevoRestaurante);
+                    System.out.println("Restaurante añadido exitosamente");
                     break;
                 case 2:
                     System.out.println("Has elegido: Editar");
